9e1173a807b41f4ff0365f9e802c50e376126a45
morqan <qurbanbeyli.murad@gmail.com>
2020-09-19 20:35:31 +0400

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
-import Post from "./Post";
-const post = new Post('webpack test')
+import Post from "@/Post";
+import "@style/style"
+import json from "@assets/json.json"
+import webpackBG from "@img/webpackBG.jpeg"
+import xml from "@assets/xml/data.xml"
+import "@style/scss"
 
+const post = new Post('webpack test', webpackBG)
+
+async function start() {
+    return await Promise.resolve('async is working')
+}
+start().then(console.log)
 console.log('post to string',post.toString())
+console.log('json:',json)
+console.log('xml:',xml)
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,130 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
 
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
+const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`
+
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
+    if(isProd) {
+      config.minimizer = [
+          new OptimizeCssAssetsPlugin(),
+          new TerserWebpackPlugin()
+      ]
+    }
+
+    return config
+}
+
+const cssLoader = extra => {
+    const loader = [
+        {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+                hmr: isDev,
+                reloadAll: true,
+                // publicPath: 'dist/style'
+            },
+        },
+        'css-loader'
+    ]
+    if (extra){
+        loader.push(extra)
+    }
+    return loader
+}
+
+console.log(isDev)
+console.log(isProd)
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     mode: 'development',
     entry: {
-        main: './index.js',
+        main: ['@babel/polyfill', './index.js'],
         analytics: './analytics.js',
     },
     output: {
-        filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, 'dist')
+        filename: filename('js'),
+        path: path.resolve(__dirname, 'dist'),
     },
+    resolve: {
+        extensions: ['.js','.css','.scss','.png','.jpg','.json'],
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            '@assets': path.resolve(__dirname, 'src/assets'),
+            '@img': path.resolve(__dirname, 'src/img'),
+            '@style': path.resolve(__dirname, 'src/style'),
+        }
+    },
+    devServer: {
+        port: 4200,
+        hot: isDev
+    },
+    optimization: optimization(),
     plugins: [
         new HTMLWebpackPlugin({
-            template: './index.html'
+            template: './index.html',
+            minify: {
+                collapseWhitespace: isProd
+            }
         }),
-        new CleanWebpackPlugin()
-    ]
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname,'src/img'),
+                    to: path.resolve(__dirname, 'dist/img')
+                },
+            ]
+        }),
+        new MiniCssExtractPlugin({
+            filename: filename('css'),
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: cssLoader()
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: cssLoader('sass-loader')
+            },
+            {
+                test: /\.(png|jpg|jpeg|svg|gif)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.xml$/,
+                use: ['xml-loader']
+            },
+            {
+                test: /\.js$/,
+                exclude: /node-modules/,
+                loader: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env'
+                        ]
+                    }
+                }
+            }
+        ]
+    }
 }
